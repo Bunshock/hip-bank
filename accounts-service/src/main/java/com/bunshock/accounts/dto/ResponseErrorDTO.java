4eb4e28f1a4dea9e0d7b3219dc366527837c1ad4
Bunshock <leomanto98@gmail.com>
2025-06-02 20:27:31 -0300

    Added exception handling for validations (MethodArgumentNotValidException) and malformed requests (HttpMessageNotReadableException). Updated response fields to ignore null values.

--- accounts-service/src/main/java/com/bunshock/accounts/dto/ResponseErrorDTO.java
+++ accounts-service/src/main/java/com/bunshock/accounts/dto/ResponseErrorDTO.java
@@ -1,18 +1,25 @@
 package com.bunshock.accounts.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter @Setter
 @SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseErrorDTO extends ResponseDTO {
 
     // Request URI
     private String apiPath;
 
-    private List<String> errors;
+    // For single message error (like common exceptions)
+    private String errorMessage;
+
+    // For catching multiple error messages (like validation errors)
+    private Map<String, String> errors;
 
 }
--- accounts-service/src/main/java/com/bunshock/accounts/dto/ResponseSuccessDTO.java
+++ accounts-service/src/main/java/com/bunshock/accounts/dto/ResponseSuccessDTO.java
@@ -1,11 +1,13 @@
 package com.bunshock.accounts.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter @Setter
 @SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseSuccessDTO<T> extends ResponseDTO {
 
     private String message;
