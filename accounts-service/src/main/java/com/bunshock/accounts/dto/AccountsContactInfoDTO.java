2b41ec7156e62904ddfa081131154b1b3b52efcf
Bunshock <leomanto98@gmail.com>
2025-06-20 20:36:13 -0300

    Fix: Accounts contact information is now properly retrieved from config server

--- accounts-service/src/main/java/com/bunshock/accounts/dto/AccountsContactInfoDTO.java
+++ accounts-service/src/main/java/com/bunshock/accounts/dto/AccountsContactInfoDTO.java
@@ -1,7 +1,17 @@
 package com.bunshock.accounts.dto;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 import java.util.List;
 import java.util.Map;
 
+@ConfigurationProperties(value = "accounts")
 public record AccountsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+
+    public AccountsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+        this.message = (message == null) ? "Welcome to HipBank accounts microservice (config server down! Information unavailable)" : message;
+        this.contactDetails = (contactDetails == null) ? Map.of("name", "unknown", "email", "unknown") : contactDetails;
+        this.onCallSupport = (onCallSupport == null) ? List.of("unknown") : onCallSupport;
+    }
+
 }
